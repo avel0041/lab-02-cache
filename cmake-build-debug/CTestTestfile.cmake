9d2a3a8aae2d10e3785a521420dacc6b8c600676
avel0041 <vel-ar@mail.ru>
2021-01-19 13:53:28 +0300

    v0

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cygdrive/d/lab-02-cache
-# Build directory: /cygdrive/d/lab-02-cache/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/cygdrive/d/lab-02-cache/cmake-build-debug/tests.exe")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/cygdrive/d/lab-02-cache/CMakeLists.txt;61;add_test;/cygdrive/d/lab-02-cache/CMakeLists.txt;0;")
